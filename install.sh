4aae92b189b43ceae99dcdfdf6d4bc3eb734692f
Brooks Patton <brooks.patton@healthscholars.com>
2023-09-25 09:46:19 -0600

    making install work

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ HASH_BEFORE_PULL=$(git rev-parse HEAD)
 git pull > /dev/null
 HASH_AFTER_PULL=$(git rev-parse HEAD)
 
-if [[ "$HASH_BEFORE_PULL" -ne "$HASH_AFTER_PULL" ]]; then
+if [[ "$HASH_BEFORE_PULL" != "$HASH_AFTER_PULL" ]]; then
   cp ./config.kdl ~/.config/zellij/config.kdl
   cp ./layouts/* ~/.config/zellij/layouts/
   echo "installed latest version of zellij config"
